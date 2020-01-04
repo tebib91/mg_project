931aee09772f893345a9adb94ca053aac3b5fad7
Dhia Eddine <dhiaeddine214@gmail.com>
2020-01-04 23:46:17 +0100

    backoffice table fixed

--- mgfront/src/app/backoffice/backoffice.component.ts
+++ mgfront/src/app/backoffice/backoffice.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
+const ELEMENT_DATA: any = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-backoffice',
   templateUrl: './backoffice.component.html',
   styleUrls: ['./backoffice.component.scss']
 })
 export class BackofficeComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   public sales = [{ name: 'sales', value: 0.81, extra: { format: 'percent' } }];
   public salesBgColor = { domain: ['#2F3E9E'] };
 
@@ -40,6 +54,10 @@ export class BackofficeComponent implements OnInit {
     }
   }
 
+  applyFilter(filterValue: string) {
+    console.log(filterValue);
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   onSelect(event) {
     console.log(event);
--- mgfront/src/app/backoffice/backoffice.module.ts
+++ mgfront/src/app/backoffice/backoffice.module.ts
@@ -4,13 +4,20 @@ import { CommonModule } from '@angular/common';
 import { BackofficeRoutingModule } from './backoffice-routing.module';
 import { BackofficeComponent } from './backoffice.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [BackofficeComponent],
   imports: [
     CommonModule,
     BackofficeRoutingModule,
-    NgxChartsModule
+    NgxChartsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class BackofficeModule { }
