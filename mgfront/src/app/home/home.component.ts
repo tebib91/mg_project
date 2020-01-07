b346a5eec4a02e76746958f64c31716a4a4bb5cc
tebib91 <tebib.ahmed0@gmail.com>
2020-01-07 22:39:34 +0100

    dhaw

--- mgfront/src/app/home/home.component.ts
+++ mgfront/src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 })
 export class HomeComponent implements OnInit {
-  questions : any;
+  questions: any;
   sliderArray: object[];
   transform: number;
   selectedIndex = 0;
@@ -18,45 +18,46 @@ export class HomeComponent implements OnInit {
     this.sliderArray = [];
     this.selectedIndex = 0;
     this.transform = 100;
-   }
+  }
 
   ngOnInit() {
     // this.getQuestion();
     this.sliderArray = [
-            {"img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year"},
-      {"img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year"},
-      {"img": "assets/backgound-2.png", "alt": "", "text": "365 Days Of weddings a year"},
-      {"img": "assets/backgound-1.png", "alt": "",  "text": "365 Days Of weddings a year"},
-      {"img": "assets/backgound-1.png", "alt": "",  "text": "365 Days Of weddings a year"},
-      {"img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year"}
+      { "img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year" },
+      { "img": "assets/backgound-2.png", "alt": "", "text": "365 Days Of weddings a year" },
+      { "img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year" },
+      { "img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year" },
+      { "img": "assets/backgound-1.png", "alt": "", "text": "365 Days Of weddings a year" }
     ];
 
   }
 
   getQuestion(): void {
     this.api.getAll('question')
-        .subscribe(questions => {
-          this.questions = questions;
-          console.log('questions',this.questions);
-          
-        });
+      .subscribe(questions => {
+        this.questions = questions;
+        console.log('questions', this.questions);
+
+      });
   }
   selected(x) {
     this.downSelected(x);
     this.selectedIndex = x;
-   }
-
-   keySelected(x) {
-    this.downSelected(x);
-    this.selectedIndex = x;
   }
 
-   downSelected(i) {
-   this.transform =  100 - (i) * 50;
-     this.selectedIndex = this.selectedIndex + 1;
+  keySelected(x) {
+    this.downSelected(x);
+    this.selectedIndex = x;
+    console.log('selectedIndex', this.selectedIndex, 'x: ', x);
+    
+  }
+
+  downSelected(i) {
+    this.transform = 100 - (i) * 50;
+    this.selectedIndex = this.selectedIndex + 1;
     //  if (this.selectedIndex > 4) {
     //    this.selectedIndex = 0;
     //  }
-   }
+  }
 
 }
